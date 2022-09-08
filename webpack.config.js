28896798abcedd2720d7c98171d50db37085b2dc
NoobSaiyan <ms19961998@gmail.com>
2022-09-08 12:34:28 +0530

    configuring webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,18 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 }
