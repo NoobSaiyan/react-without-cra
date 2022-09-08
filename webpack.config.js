4b921fa99188b1c5bef1fc4aedccce715fad72f9
NoobSaiyan <ms19961998@gmail.com>
2022-09-08 12:30:13 +0530

    setting output  and template html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
